81cf6197f574260402d6466fb839d7438659e6ff
Ian Wren <ian.wren81@gmail.com>
2020-08-24 18:29:51 -0700

    Added Javascript and javascript tags to page

---
+++ Javascript.js
@@ -0,0 +1,21 @@
+document.getElementById("button1).addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "250px";
+
+});
+
+document.getElementById("button2").addEventListener("click", function(){
+
+    document.getElementById("box").style.color = "Blue";
+
+});
+
+document.getElementById("button3").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "150px";
+
+});
+
+document.getElementById("button4").addEventListener("click", function(){
+    document.getElementById("box").style.height = "150px";
+});
